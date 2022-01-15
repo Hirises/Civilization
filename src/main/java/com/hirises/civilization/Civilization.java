21e5dd651109819eab36e2080329175602fe20eb
Hirises <hirises1243@gmail.com>
2022-01-15 17:40:18 +0900

    init

---
+++ src/main/java/com/hirises/civilization/Civilization.java
@@ -0,0 +1,17 @@
+package com.hirises.civilization;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Civilization extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
