a8d2e152fcfa03123c44577175e22126440f1ca8
Hirises <hirises1243@gmail.com>
2022-01-15 17:43:56 +0900

    의존성 추가

--- src/main/java/com/hirises/civilization/Civilization.java
+++ src/main/java/com/hirises/civilization/Civilization.java
@@ -4,14 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Civilization extends JavaPlugin {
 
+    private static Civilization plugin;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        plugin = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Civilization getInst() {
+        return plugin;
     }
 }
