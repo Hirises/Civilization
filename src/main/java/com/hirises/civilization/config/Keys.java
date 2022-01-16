98281dd7ccfd82ef597b820cd6b6fc0776051615
Hirises <hirises1243@gmail.com>
2022-01-16 22:00:20 +0900

    아이템 선별 폐기

--- src/main/java/com/hirises/civilization/config/Keys.java
+++ src/main/java/com/hirises/civilization/config/Keys.java
@@ -1,8 +1,6 @@
 package com.hirises.civilization.config;
 
 public enum Keys{
-    EnableItem("civilization_item_enabled"),
-    ProjectileLauncher("civilization_projectile_lunch"),
     ;
 
     private String key;
